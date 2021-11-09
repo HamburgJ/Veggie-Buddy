6f64320a4f2a4a3b467903ccd1123595dd106994
Joshua <jhamburg@uwaterloo.ca>
2021-11-09 15:54:44 -0500

    make useful constants and dicts

--- constants.py
+++ constants.py
@@ -30,7 +30,9 @@ owned_by_loblaws = [
     'Real Canadian Superstore',
     'Zehrs',
     'T&T',
-    'Independent'
+    'Independent',
+    'Freshmart',
+    'Valumart'
 ]
 
 owned_by_metro = [
@@ -43,8 +45,43 @@ owned_by_empire = [
     'Farm Boy',
     'Freshco',
     'Longos',
-    'Sobeys'
+    'Sobeys',
+    'Foodland'
 ]
+
+name_replacements = {
+    "®": "",
+    'mangos': "mangoes",
+    "sweet potatoes": "yams",
+    "jalapeño pepper": "jalepeno",
+    'chili pepper': 'chili',
+    'clementine': 'orange',
+    'mandarin': 'orange',
+    'salads': 'salad' 
+}
+
+price_replacements = {
+    "¢": "",
+    'or less than 2': 'or',
+    'or less than 3': 'or',
+    'or less than 4': 'or',
+    'or less than 5': 'or',
+    'or less than 6': 'or',
+    'less than 2': 'or',
+    'less than 3': 'or',
+    'less than 4': 'or',
+    'less than 5': 'or',
+    'less than 6': 'or',
+    'each': 'ea.',
+    ' ea.': "",
+    ' or /lb': "",
+    '/lb': 'lb',
+    " .": " $0.",
+    ' ea': '',
+    " for": "/",
+    ' ealb': '',
+    'lb.': 'lb'
+}
 
 vegan_keywords = [
     'violife',
@@ -144,13 +181,14 @@ meat_keywords = [
     'cake',
     'whites',
     'fish',
-    'yogourt',
-    'yoplait',
     'fillets',
     'steak',
     'parlour',
     'pogo',
-    'entrees',
+    'entrees'
+]
+
+dairy_keywords = [
     'iögo',
     'crema',
     'nestlé pouches',
@@ -159,7 +197,9 @@ meat_keywords = [
     'chool whip',
     'skyr',
     'cheestrings',
-    'black diamond'
+    'black diamond',
+    'yogourt',
+    'yoplait',
 ]
 
 non_vegan = [
@@ -246,3 +286,623 @@ postal_codes = {
     'Guelph': 'N1E2L8'
 }
 '''
+
+item_match_dict = {
+    "cola": {
+        "category": 'other',
+        "positive": [
+            'coke',
+            'pepsi',
+            'cola',
+            'soft drink',
+            'soda'
+        ],
+        "negative": [
+            'soda water'
+        ]
+    },
+    "becel": {
+        "category": 'vegan',
+        "postive":[
+            'becel'
+        ],
+        "negative": []
+    },
+    "coffee": {
+        "category": 'other',
+        "postive":[
+            'coffee',
+            'roast and ground',
+            'maxwell house',
+            'nabob'
+        ],
+        "negative": []
+    },
+    "nut milk": {
+        "category": 'vegan',
+        "postive":[
+            'coconut dream',
+            'silk',
+            'blue diamond',
+            'almond milk',
+            'oat yeah',
+            'soy milk',
+            'not milk',
+            'oat milk',
+            'almond milk',
+            'cashew milk'
+            'soy beverage',
+            'almond beverage',
+            'oat beverage',
+            'coconut beverage',
+            'cashew beverage',
+            'coconut milk',
+            'cashew milk',
+            'pea milk',
+            'plant based milk',
+            'plant-based milk',
+            'plant milk',
+        ],
+        "negative": [
+            'becel',
+            'pudding',
+        ]
+    },
+    "chips": {
+        "category": 'other',
+        "postive":[
+            'chips',
+            'cheetos',
+            'bugels',
+            'doritos',
+            'lays',
+            'corn chips',
+            'kettle chips',
+            'kettle cooked chips'
+            'poppables'
+        ],
+        "negative": [
+            'chocolate chips',
+            'carob chips'
+        ]
+    },
+    "popcorn": {
+        "category": 'other',
+        "postive":[
+            'popcorn',
+            'smartfood',
+            'bad monkey',
+            'caramel corn',
+            'chicago corn',
+            'pop corn'
+        ],
+        "negative": [
+
+        ]
+    },
+    "crackers": {
+        "category": 'other',
+        "postive":[
+            'crackers',
+            'triscuit',
+            'wheat thins',
+            'crispers',
+            'ritz',
+            'saltine',
+            'saltines',
+            'cream cracker',
+        ],
+        "negative": []
+    },
+    "cereal": {
+        "category": 'other',
+        "postive":[
+            'cereal',
+            'cereals',
+            'general mills',
+            'shreddies',
+            'post'
+        ],
+        "negative": [
+            'oatmeal',
+            'cake'
+        ]
+    },
+    "oats": {
+        "category": 'other',
+        "postive":[
+            'oatmeal',
+            'oats'
+        ],
+        "negative": [
+            'of oats'
+        ]
+    },
+    "": {
+        "category": '',
+        "postive":[
+            
+        ],
+        "negative": [
+            
+        ]
+    },
+    "chocolate": {
+        "category": 'other',
+        "postive":[
+            'chocolate',
+            'chocolates'
+        ],
+        "negative": [
+            'chocolate milk',
+            'chocolate ice',
+            'chocolate cream',
+            'chocolate cake',
+            'chocolate brownie',
+            'chocolate cookie',
+            'chocolate pastry',
+            'chocolate cereal',
+            'chocolate bread',
+            'chocolate bun'
+        ]
+    },
+    "salad dressing": {
+        "category": 'other',
+        "postive":[
+            'dressing',
+            'dressings'
+        ],
+        "negative": []
+    },
+    "salad": {
+        "category": 'produce',
+        "postive":[
+            'salad',
+            'salads'
+        ],
+        "negative": [
+            'dressing',
+            'dressings',
+            'fruit salad',
+            'salad mix',
+            'pasta salad'
+        ]
+    },
+    "corn": {
+        "category": 'produce',
+        "postive":[
+            'corn'
+        ],
+        "negative": [
+            'beef',
+            'chip',
+            'chips',
+            'tortilla',
+            'tortillas',
+            'popping',
+            'pop',
+            'cereal',
+            'milk',
+            'salad',
+            'mix'
+        ]
+    },
+    "onion": {
+        "category": 'produce',
+        "postive":[
+            'onion',
+            'onions'
+        ],
+        "negative": [
+            'green onion',
+            'green onions',
+            'red onion',
+            'red onions',
+            'powder',
+            'seasoning',
+            'chips',
+            'dip',
+            'popcorn',
+            'dried',
+            'caramelized'
+        ]
+    },
+    "green onion": {
+        "category": 'produce',
+        "postive":[
+            'green onion',
+            'green onions'
+        ],
+        "negative": [
+            'powder',
+            'seasoning',
+            'dried',
+            'popcorn',
+            'chips'
+        ]
+    },
+    "red onion": {
+        "category": 'produce',
+        "postive":[
+            'red onion',
+            'red onions'
+        ],
+        "negative": [
+            'salad'
+        ]
+    },
+    "green beans": {
+        "category": 'produce',
+        "postive":[
+            'green bean',
+            'green beans',
+            'long beans',
+            'fresh beans',
+            'french beans'
+        ],
+        "negative": [
+            'canned',
+            'pickled'
+        ]
+    },
+    "lettuce": {
+        "category": 'produce',
+        "postive":[
+            'romaine',
+            'lettuce'
+        ],
+        "negative": [
+            'salad',
+            'wrap',
+            'roll',
+            'sandwich'
+        ]
+    },
+    "sweet potato": {
+        "category": 'produce',
+        "postive":[
+            'yams',
+            'sweet potato',
+            'yam',
+            'sweet potatoes'
+        ],
+        "negative": [
+            'pie',
+            'food',
+            'sauce',
+            'baby'
+        ]
+    },
+    "pasta sauce": {
+        "category": 'other',
+        "postive":[
+            'classico',
+            'pasta sauce',
+            'prima',
+            'pasta sauces',
+            'marinara sauce',
+            'marinara sauces',
+            'tomato sauce'
+        ],
+        "negative": []
+    },
+    "fruit snack": {
+        "category": 'other',
+        "postive":[
+            'fruit bowls',
+            'fruit bowl',
+            'dole fruit',
+            'fruit cup',
+            'fruit cups',
+            'applesauce'
+        ],
+        "negative": []
+    },
+    "peanut butter": {
+        "category": 'other',
+        "postive":[
+            'peanut butter',
+            'jif'
+        ],
+        "negative": []
+    },
+    "jam": {
+        "category": 'other',
+        "postive":[
+            "jam",
+            "jelly"
+        ],
+        "negative": [
+            "jellies"
+        ]
+    },
+    "ketchup": {
+        "category": 'other',
+        "postive":[
+            'ketchup'
+        ],
+        "negative": [
+            'chips',
+            'chip',
+        ]
+    },
+    "mustard": {
+        "category": 'other',
+        "postive":[
+            'mustard'
+        ],
+        "negative": [
+            'pretzel',
+            'pretzels',
+            'chip',
+            'chips'
+        ]
+    },
+    "vinegar": {
+        "category": 'other',
+        "postive":[
+            'vinegar'
+        ],
+        "negative": [
+            'salt and',
+            'chips',
+            'crisps',
+            'fish'
+        ]
+    },
+    "sugar": {
+        "category": 'other',
+        "postive":[
+            'white sugar',
+            'brown sugar',
+            'icing sugar'
+        ],
+        "negative": [
+            'oatmeal',
+            'cereal',
+            'cake',
+            'pie'
+        ]
+    },
+    "sparkling water": {
+        "category": 'other',
+        "postive":[
+            'sparkling water',
+            'flavoured water',
+            'flavored water',
+            'stevia water',
+            'fizzy wayer',
+            'bubly',
+            'montelliar',
+            'free and clear',
+            'carbonated water'
+        ],
+        "negative": [
+            'water bottle',
+            'water bottles',
+            'bottled water'
+        ]
+    },
+    "bagels": {
+        "category": 'bakery',
+        "postive":[
+            'bagel',
+            'bagels'
+        ],
+        "negative": [
+            'hummus',
+            'bagel bite',
+            'bagel bites',
+            'seasoning',
+            'mini',
+            ''
+        ]
+    },
+    "english muffins": {
+        "category": 'bakery',
+        "postive":[
+            'english muffins',
+            'english muffin'
+        ],
+        "negative": [
+            'maple',
+            'cinnamon'
+        ]
+    },
+    "bread": {
+        "category": 'bakery',
+        "postive":[
+            'bread',
+            'loaf'
+        ],
+        "negative": [
+            'baguette',
+            'bun',
+            'buns',
+            'bowl'
+        ]
+    },
+    "tortillas": {
+        "category": 'bakery',
+        "postive":[
+            'tortilla',
+            'tortillas'
+        ],
+        "negative": [
+            'chips',
+            'seasoning',
+            'hard',
+            'fried'
+        ]
+    },
+    "hummus": {
+        "category": 'vegan',
+        "postive":[
+            'hummus',
+            'chickpea dip'
+        ],
+        "negative": []
+    },
+    "mixed nuts": {
+        "category": 'other',
+        "postive":[
+            'mixed nuts',
+            'nut mix',
+            'premium nuts',
+            'premium mixed nuts',
+            'deluxe nuts',
+            'deluxe mixed nuts'
+        ],
+        "negative": []
+    },
+    "cookies": {
+        "category": 'bakery',
+        "postive":[
+            'cookies',
+            'cookie',
+            'oreo',
+            'chips ahoy',
+            'voortman'
+        ],
+        "negative": [
+            
+        ]
+    },
+    "oil": {
+        "category": 'other',
+        "postive":[
+            'olive oil',
+            'canola oil',
+            'corn oil',
+            'vegetable oil',
+            'seed oil'
+        ],
+        "negative": [
+            'hummus',
+            'chips',
+            'chip',
+            'olives',
+            'tomatoes',
+            'salad',
+            'mix'
+        ]
+    },
+    "broth": {
+        "category": 'other',
+        "postive":[
+            'broth'
+        ],
+        "negative": []
+    },
+    "pickes": {
+        "category": 'other',
+        "postive":[
+            'pickles',
+            'gherkins'
+        ],
+        "negative": [
+            'chips',
+            'flavour',
+            'flavor',
+            'flavored',
+            'flavoured'
+        ]
+    },
+    "buns": {
+        "category": 'bakery',
+        "postive":[
+            'buns'
+        ],
+        "negative": []
+    },
+    "canned tomatoes": {
+        "category": 'other',
+        "postive":[
+            'canned tomato',
+            'canned tomatoes',
+            'rotel',
+            'tomato paste',
+            'crushed tomato'
+        ],
+        "negative": []
+    },
+    "salsa": {
+        "category": 'other',
+        "postive":[
+            'salsa'
+        ],
+        "negative": []
+    },
+    "tofu": {
+        "category": 'vegan',
+        "postive":[
+            'tofu'
+        ],
+        "negative": [
+            'dip',
+            'noodles'
+        ]
+    },
+    "waffles": {
+        "category": 'other',
+        "postive":[
+            'waffles',
+            'waffle',
+            'eggo'
+        ],
+        "negative": [
+            'belgian'
+        ]
+    },
+    "oranges": {
+        "category": 'produce',
+        "postive":[
+            'orange',
+            'oranges'
+        ],
+        "negative": [
+            'segments',
+            'canned',
+            'syrup',
+            'candy',
+            'sours',
+            'gummies',
+            'popcicles',
+            'popcicle',
+            'ice cream',
+            'sauce',
+            'juice',
+            'pekoe',
+            'drink',
+            'tropicana',
+            'soda',
+            'pop',
+            'sodas',
+            'sparkling',
+            'cookie',
+            'cookies',
+            'pulp',
+            'pure',
+            'marmalade',
+            'water',
+            'fanta',
+            'tic tac',
+            'zevia',
+            'gatorade',
+            'peel',
+            'crush',
+            'jelly',
+            'jellies',
+            'jam',
+            'jello',
+            'jell-o',
+            'powder',
+            'crystals',
+            'tang',
+            'bar',
+            'soap',
+            'shampoo',
+            'chocolate'
+        ]
+    }
+}
